a1ae9cbd294f79633dde75d7e4817e1698c0188b
Dioo16 <borgesdiogo54@gmail.com>
2024-08-12 21:47:50 -0300

    add headless to init custom selenium

--- src/frameworks_drivers/drivers/selenium_driver.py
+++ src/frameworks_drivers/drivers/selenium_driver.py
@@ -26,6 +26,7 @@ class CustomSelenium:
 
         try:
             chrome_options = Options()
+            chrome_options.add_argument("--headless")
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument("--disable-extensions")
             chrome_options.add_argument("--disable-gpu")
