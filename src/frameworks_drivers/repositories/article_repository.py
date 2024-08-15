23d4ee31ec4ac0251ca6df9a109c997cd0421f59
Dioo16 <borgesdiogo54@gmail.com>
2024-08-15 15:54:10 -0300

    change path xlsx

--- src/frameworks_drivers/repositories/article_repository.py
+++ src/frameworks_drivers/repositories/article_repository.py
@@ -31,7 +31,7 @@ class ArticleRepository(ArticleRepositoryInterface):
     def define_xlsx_filename(phrase_searched: str, dir: str, month: int) -> str:
         from_date = utils.date_utils.return_current_month_plus_next_months(month-1).strftime("%m-%d-%Y")
         to_date = datetime.now().strftime("%m-%d-%Y")
-        return f"{dir}\\news_search_{phrase_searched}_{from_date}_to_{to_date}.xlsx"
+        return f"{dir}/news_search_{phrase_searched}_{from_date}_to_{to_date}.xlsx"
 
 
 def define_output_dir_to_save_excel():
